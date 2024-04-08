f76a9cae341e0d9ea4b358945abd98fb855c519c
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-08 12:37:58 +0200

    Update 2-main.js

--- 0x00-ES6_basic/2-main.js
+++ 0x00-ES6_basic/2-main.js
@@ -1,5 +1,5 @@
 import getNeighborhoodsList from './2-arrow.js';
 
-const neighborhoodsList = new getNeighborhoodsList();
+const neighborhoodsList = getNeighborhoodsList();
 const res = neighborhoodsList.addNeighborhood('Noe Valley');
 console.log(res);
