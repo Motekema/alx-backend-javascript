e8442acca05877146395d2d391d59ae5cfcffc36
Motekema <motekema@gmail.com>
2024-04-08 03:31:51 -0700

    My commit

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+const getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+};
+
+export default getNeighborhoodsList;
