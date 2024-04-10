13a27fef8bb57071d0b2f6d964c6173e2b1b23c6
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-10 15:57:30 +0200

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,3 @@
-function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
-  });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
-
-export default signUpUser;
