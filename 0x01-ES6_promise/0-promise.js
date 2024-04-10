0f59d397be3e41f1bf14456c869b318798b4b4bc
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-10 15:28:28 +0200

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,4 @@
 export default getResponseFromAPI;
 export default function getResponseFromAPI() {
-  // Your asynchronous code here
-    // For example, fetching data from an API
-    // For demonstration purposes, resolve with a dummy response
   return new Promise(() => {});
 }
