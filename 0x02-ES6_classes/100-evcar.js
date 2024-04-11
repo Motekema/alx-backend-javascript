13cd1b8f775fbd8d6f4dcc407b413fe51046be03
Motekema <motekema@gmail.com>
2024-04-11 05:06:51 -0700

    My commit

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+// 100-evcar.js
+import Car from './10-car.js';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
+
+export default EVCar;
