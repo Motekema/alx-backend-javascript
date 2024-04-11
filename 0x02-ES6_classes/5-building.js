c799dcd231c6dd49ffd2486b22a359f2c722de5a
Motekema <motekema@gmail.com>
2024-04-11 04:49:27 -0700

    My commit

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+// 5-building.js
+
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
