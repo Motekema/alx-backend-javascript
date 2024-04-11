2ccf89ab5ac757c3a78203af483554f17792eab3
Motekema <motekema@gmail.com>
2024-04-11 04:55:54 -0700

    My commit

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,22 @@
+// 7-airport.js
+
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
+
+export default Airport;
