70a0557e56259e0b1bd8f76f1350b5d7222e1ac6
Motekema <motekema@gmail.com>
2024-04-11 04:41:27 -0700

    My commit

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+// 3-currency.js
+
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
