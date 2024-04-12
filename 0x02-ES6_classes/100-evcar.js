b37c03629a2fa5b5060379ef45902eb45cb61222
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-12 20:42:05 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,24 @@
 // 100-evcar.js
 import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
   }
 
   get range() {
     return this._range;
   }
 
+  set range(value) {
+    this._range = value;
+  }
+
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
-
-export default EVCar;
 
