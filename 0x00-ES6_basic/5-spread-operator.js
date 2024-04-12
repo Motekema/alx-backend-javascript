61462b4396f17767fe5acf318e618a0a878259fe
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-12 19:10:34 +0200

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
-const concatArrays = (array1, array2, string) => {
+export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
-};
-
-export default concatArrays;
+}
