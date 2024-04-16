c413656ed34baa885a2d4061b1e70639bba699b8
Motekema <motekema@gmail.com>
2024-04-16 02:28:13 -0700

    My commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+// 0-get_list_students.js
+
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
+
+export default getListStudents;
