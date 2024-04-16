2dcc42f5b0e04c92c063ead2b1976352bc492326
Motekema <motekema@gmail.com>
2024-04-16 03:02:45 -0700

    My commit

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+// 5-typed_arrays.js
+
+function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
+}
+
+export default createInt8TypedArray;
