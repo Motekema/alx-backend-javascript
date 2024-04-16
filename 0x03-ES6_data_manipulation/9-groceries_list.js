094c3fa17473794b5cda6c5b98fe284d60d0c8e9
Motekema <motekema@gmail.com>
2024-04-16 03:15:18 -0700

    My commit

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+// 9-groceries_list.js
+
+function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
+}
+
+export default groceriesList;
