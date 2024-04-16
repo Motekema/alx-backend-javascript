d3854ea338ad94b0b52310d8bda4286294b9b079
Teki Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-04-16 11:30:53 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,9 @@
 // 0-get_list_students.js
 
-function getListStudents() {
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
-
-export default getListStudents;
