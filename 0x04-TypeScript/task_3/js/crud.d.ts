9cf69383ab256241c7ac3c42bd0f57882daa74ad
Motekema <motekema@gmail.com>
2024-04-17 05:13:40 -0700

    My commit

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,9 @@
+// crud.d.ts
+
+import { RowID, RowElement } from './interface';
+
+declare module 'crud.js' {
+  export function insertRow(row: RowElement): RowID;
+  export function deleteRow(rowId: RowID): void;
+  export function updateRow(rowId: RowID, row: RowElement): RowID;
+}
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// interface.ts
+
+export type RowID = number;
+
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
