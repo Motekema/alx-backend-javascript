c674cad830506efb2d3b3b5cba656307da01ee55
Motekema <motekema@gmail.com>
2024-06-04 00:51:28 -0700

    My commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Return a message on the STDOUT.
+ * @param {String} msg a message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage
