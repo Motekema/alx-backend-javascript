b8599e2c94e7e1e323554fd247b838a76e988475
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-04 09:54:49 +0200

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,10 @@
 /**
  * Return a message on the STDOUT.
  * @param {String} msg a message to display.
+ * @author Motekema Mahlanya <https://github.com/Motekema>
  */
 const displayMessage = (msg) => {
   console.log(msg);
 };
 
-module.exports = displayMessage
+module.exports = displayMessage;
