142009d3f93dae1400d7c59dfa3734715a81f231
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:15:20 +0200

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,15 @@
+/**
+ * Adds two numbers after rounding them to the nearest integer.
+ *
+ * The function takes two arguments, rounds each to the nearest integer
+ * using the built-in Math.round() method, and then returns the sum of these
+ * rounded values.
+ *
+ * @param {number} a - The first number to round and add.
+ * @param {number} b - The second number to round and add.
+ * @returns {number} The sum of the rounded numbers.
+ */
 const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
+
