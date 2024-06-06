db1a0f0a407cdf7e6ad79c737ce2128c77b52953
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:26:56 +0200

    Update 2-calcul_chai.js

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,3 +1,12 @@
+/**
+ * Performs a specified operation on two numbers after rounding them.
+ *
+ * @param {string} type - The type of operation to perform. It can be 'SUM', 'SUBTRACT', or 'DIVIDE'.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number|string} The result of the operation. Returns a number for 'SUM' and 'SUBTRACT'.
+ * For 'DIVIDE', returns the quotient or 'Error' if the divisor is 0.
+ */
 const calculateNumber = (type, a, b) => {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
