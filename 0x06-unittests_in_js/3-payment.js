42c9c6e2414688ebea35e678c8b4fb6c3946f613
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:33:57 +0200

    Update 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,15 @@
 const Utils = require('./utils');
 
+/**
+ * Calculates the total payment amount and logs the result.
+ *
+ * @param {number} totalAmount - The total amount for the payment.
+ * @param {number} totalShipping - The total shipping cost.
+ */
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${totalCost}`);
 };
 
 module.exports = sendPaymentRequestToApi;
+
