c66453c853a2d181063ada7c28d94acb030563b5
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:34:48 +0200

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,12 +4,22 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
+  /**
+   * Test to verify that the sendPaymentRequestToApi function uses the calculateNumber method of Utils.
+   */
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    // Create a spy for the calculateNumber method of Utils
     const bigBrother = sinon.spy(Utils);
 
+    // Call the sendPaymentRequestToApi function
     sendPaymentRequestToApi(100, 20);
+
+    // Check if the calculateNumber method was called with the correct arguments
     expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    // Check if the calculateNumber method was called exactly once
     expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+
+    // Restore the original calculateNumber method
     bigBrother.calculateNumber.restore();
   });
 });
