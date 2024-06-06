58807bdc668384a9fc0bbec439f0c07627e94f09
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:43:51 +0200

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,18 @@
+// Importing necessary modules and dependencies
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
+// Test suite for getPaymentTokenFromAPI function
 describe('getPaymentTokenFromAPI', () => {
+  // Test case for verifying the response when the API request is successful (success = true)
   it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+    // Call getPaymentTokenFromAPI function with success = true
     getPaymentTokenFromAPI(true)
+      // Promise chain to handle the asynchronous response
       .then((res) => {
-        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        // Assertion to check if the response matches the expected response
+        expect(res).to.deep.equal({ data: 'Successful response from the API' });
+        // Call the done() callback to signal the completion of the asynchronous test
         done();
       });
   });
