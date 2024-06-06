c69d638f828302cd8140e8a48b191b6df047ffa0
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 16:07:27 +0200

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,23 @@
+// Importing the express module
 const express = require('express');
 
+// Creating an Express application
 const app = express();
-const PORT = 7865;
+const PORT = 7865; // Defining the port number
 
+/**
+ * GET endpoint for the root URL ('/')
+ * Sends a welcome message for the payment system
+ */
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
+// Starting the server and listening on the defined port
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
+// Exporting the Express application for use in other modules or for testing
 module.exports = app;
+
