e095fce9a2599cdd22d4eb4f3090ab7bdb9772b0
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:43:12 +0200

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,14 @@
+/**
+ * Simulates a request to get a payment token from an API.
+ * @param {boolean} success - Indicates whether the request to the API is successful or not.
+ * @returns {Promise} - A Promise that resolves with the payment token if the request is successful.
+ */
 const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  // If the request is successful, resolve the Promise with a mock response
   if (success) {
-    resolve({data: 'Successful response from the API'});
+    resolve({ data: 'Successful response from the API' });
   }
 });
 
+// Exporting the function for use in other modules
 module.exports = getPaymentTokenFromAPI;
