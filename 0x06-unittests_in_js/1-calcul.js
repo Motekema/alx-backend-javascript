2927135f23432e6c04b7244a7e15786bbbe2a636
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-06 15:21:30 +0200

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,24 @@
+/**
+ * Function that performs a mathematical operation on two numbers after rounding them.
+ * @param {string} type - The type of operation to perform ('SUM', 'SUBTRACT', 'DIVIDE').
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number|string} - The result of the operation or 'Error' if division by zero.
+ */
 const calculateNumber = (type, a, b) => {
   if (type === 'SUM') {
+    // Rounds both numbers and returns their sum
     return Math.round(a) + Math.round(b);
   }
   if (type === 'SUBTRACT') {
+    // Rounds both numbers and returns their difference
     return Math.round(a) - Math.round(b);
   }
   if (type === 'DIVIDE') {
+    // Rounds both numbers and returns their quotient or 'Error' if division by zero
     return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
+  // Returns 0 if the type is not recognized
   return 0;
 };
 
