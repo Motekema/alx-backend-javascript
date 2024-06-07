d00df05da7fd5c1fb26bc1f7ec96fa52df428330
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-07 18:14:21 +0200

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -5,7 +5,7 @@ import StudentsController from '../controllers/StudentsController';
  * Binds the routes to the appropriate handler in the
  * given Express application.
  * @param {Express} app The Express application.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Motekema Mahlanya <https://github.com/Motekema>
  */
 const mapRoutes = (app) => {
   app.get('/', AppController.getHomepage);
